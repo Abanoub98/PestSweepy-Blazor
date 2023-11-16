780c35e1d1fbbc8fa47d09bed5c1b22d42412467
youssef ahmed <youssefahmedabdelfattah@outlook.com>
2023-11-16 12:13:56 +0200

    minor

--- Dashboard.Blazor/Models/Consts/Placeholders.cs
+++ Dashboard.Blazor/Models/Consts/Placeholders.cs
@@ -5,7 +5,7 @@ public class Placeholders
     public const string Name = "Name";
     public const string PhoneNumber = "05147*****";
     public const string DurationInMinutes = "Duration";
-    public const string OrderIndex = "Order Index";
+    public const string OrderIndex = "Order";
     public const string GenderDropDown = "Select Gender";
     public const string ManagerDropDown = "Select Manager";
     public const string SupervisorDropDown = "Select Supervisor";
