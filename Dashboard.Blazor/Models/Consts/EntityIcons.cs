ef9cfb2fd864cf452cb9b907602d5ed27ad34ba4
abanoub98 <bebo_dragon99@yahoo.com>
2024-01-11 04:42:52 +0200

    Enhance Sidebar icons and sorting & edit contract table

--- Dashboard.Blazor/Models/Consts/EntityIcons.cs
+++ Dashboard.Blazor/Models/Consts/EntityIcons.cs
@@ -2,10 +2,10 @@
 
 public static class EntityIcons
 {
-    public const string ContractsIcon = Icons.Material.TwoTone.FolderShared;
     public const string CompanyInfoIcon = Icons.Material.TwoTone.Info;
-    public const string QuotationsIcon = Icons.Material.TwoTone.FilePresent;
+    public const string QuotationsIcon = "fa-solid fa-table-list";
     public const string CurrencyIcon = "fa-solid fa-coins";
     public const string ReviewIcon = Icons.Material.TwoTone.RateReview;
     public const string CouponIcon = @Icons.Material.TwoTone.Discount;
+    public const string ContractsIcon = "fa-solid fa-file-signature";
 }
