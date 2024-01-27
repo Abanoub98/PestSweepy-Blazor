47eaa9349ef17390918b732479e4e2047d80a9cb
youssef ahmed <youssefahmedabdelfattah@outlook.com>
2024-01-27 21:58:35 +0200

    Update Forms

---
+++ Dashboard.Blazor/Models/Dtos/DashboardDto.cs
@@ -0,0 +1,18 @@
+﻿namespace Dashboard.Blazor.Models.Dtos;
+
+public class DashboardDto
+{
+    public int ServicesCount { get; set; }
+    public int ClientsCount { get; set; }
+    public int ContractClientsCount { get; set; }
+    public int SupervisorsCount { get; set; }
+    public int ManagersCount { get; set; }
+    public List<Earning> Earnings { get; set; } = new();
+}
+
+public class Earning
+{
+    public double Amount { get; set; }
+    public string CurrencyCode { get; set; } = null!;
+    public string CurrencySymbol { get; set; } = null!;
+}
