bb1f50eef88b4e264d5d3fa3f96f8f4e0e896ec9
abanoub98 <bebo_dragon99@yahoo.com>
2026-05-10 01:00:50 +0300

    complaint dto

--- Dashboard.Blazor/Models/Dtos/ComplaintDto.cs
+++ Dashboard.Blazor/Models/Dtos/ComplaintDto.cs
@@ -8,6 +8,12 @@ public class ComplaintDto
     public string Subject { get; set; } = null!;
     public bool IsResolved { get; set; }
     public ClientDto Client { get; set; } = new();
+    public int ComplaintTypeId { get; set; }
     public LookupDto ComplaintType { get; set; } = new();
+    public IEnumerable<LookupDto> ComplaintTypes { get; set; } = null!;
+    public int BranchId { get; set; }
+    public BranchBaseDto Branch { get; set; } = null!;
+    public IEnumerable<BranchBaseDto> Branches { get; set; } = null!;
+    public IBrowserFile? UploadedImage { get; set; }
     public string Image { get; set; } = null!;
 }
